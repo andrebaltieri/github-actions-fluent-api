e58ed45b2b841b35b55a2b49199c7a34ff0c7d27
Andre Baltieri <andrebaltieri@hotmail.com>
2021-08-07 11:54:47 -0300

    Hello world

---
+++ GitHub/GitHubActionBuilder.cs
@@ -0,0 +1,68 @@
+﻿namespace ActionsBuilder.GitHub
+{
+    public class GitHubActionBuilder : IWorkflow, IWorkflowName, IBranchName, IRunsOn, IStep, IBuild
+    {
+        private GitHubActionBuilder()
+        {
+        }
+
+        public static IWorkflow Configure()
+        {
+            return new GitHubActionBuilder();
+        }
+
+        public IWorkflowName WithName(string name)
+        {
+            return this;
+        }
+
+        public IBranchName OnBranch(string name)
+        {
+            return this;
+        }
+
+        public IRunsOn RunsOn(string name)
+        {
+            return this;
+        }
+        
+        public IStep AddStep(string name, string run)
+        {
+            return this;
+        }
+
+        public IBuild Build()
+        {
+            return this;
+        }
+    }
+
+    public interface IWorkflow
+    {
+        public IWorkflowName WithName(string name);
+    }
+
+    public interface IWorkflowName
+    {
+        public IBranchName OnBranch(string name);
+    }
+
+    public interface IBranchName
+    {
+        public IRunsOn RunsOn(string name);
+    }
+
+    public interface IRunsOn
+    {
+        public IStep AddStep(string name, string run);
+    }
+
+    public interface IStep
+    {
+        public IBuild Build();
+    }
+
+    public interface IBuild
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ActionsBuilder.GitHub;
+
+namespace ActionsBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var workflow = GitHubActionBuilder
+                .Configure()
+                .WithName("My workflow name")
+                .OnBranch("main")
+                .RunsOn("ubuntu-latest")
+                .AddStep("Setup .NET", "actions/setup-dotnet@v1")
+                .Build();
+        }
+    }
+}
